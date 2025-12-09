c155293f9263449fb7d2722c31b429c75ac85bf2
Gunnar <160498180+GunnarWaem@users.noreply.github.com>
2025-12-09 17:26:57 +0100

    Initial commit

---
+++ src/main/java/net/gsaurus/missingblocks/MissingBlocks.java
@@ -0,0 +1,16 @@
+package net.gsaurus.missingblocks;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MissingBlocks implements ModInitializer {
+	public static final String MOD_ID = "missingblocks";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/gsaurus/missingblocks/MissingBlocksClient.java
@@ -0,0 +1,10 @@
+package net.gsaurus.missingblocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MissingBlocksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
